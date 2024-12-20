d03a5c27846b17e4c67579246c597d213516efb8
KateGaga <kate10111204@gmail.com>
2024-12-20 15:49:08 +0800

    your commit content.

---
+++ app/src/main/java/com/example/lab12/UnsafeOkHttpCilent.java
@@ -0,0 +1,47 @@
+package com.example.lab12;
+
+import java.security.*;
+import java.security.cert.CertificateException;
+import java.security.cert.X509Certificate;
+
+import javax.net.*;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.X509TrustManager;
+
+import okhttp3.OkHttpClient;
+
+
+public class UnsafeOkHttpCilent {
+    public static OkHttpClient getUnsafeOkHttpClient(){
+        try{
+            final TrustManager[] trustAllCerts = new TrustManager[]{
+                    new X509TrustManager() {
+                        @Override
+                        public void checkClientTrusted(X509Certificate[] x509Certificates, String s) throws CertificateException {
+
+                        }
+
+                        @Override
+                        public void checkServerTrusted(X509Certificate[] x509Certificates, String s) throws CertificateException {
+
+                        }
+
+                        @Override
+                        public X509Certificate[] getAcceptedIssuers() {
+                            return new X509Certificate[]{};
+                        }
+                    }
+
+            };
+            final SSLContext sslContext = SSLContext.getInstance("SSL");
+            sslContext.init(null,trustAllCerts,new SecureRandom());
+            return  new OkHttpClient
+                    .Builder()
+                    .sslSocketFactory(sslContext.getSocketFactory(),(X509TrustManager) trustAllCerts[0])
+                    .build();
+        }catch (Exception e){
+            throw new RuntimeException();
+        }
+    }
+}
